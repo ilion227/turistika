283fddff0eb0c2ae3b70a488a564f90f2922e762
Franci <franciklavz@gmail.com>
2016-05-12 13:52:58 +0200

    travels added

---
+++ travel.php
@@ -0,0 +1,19 @@
+<?php
+    include_once 'database.php';
+    include_once 'session.php';
+    
+    $id = (int) $_GET['id'];
+    
+    $query = "SELECT * FROM destinations WHERE id = $id";
+    $result = mysqli_query($link, $query);
+    $destination = mysqli_fetch_array($result);
+    
+    echo "<h2>Odločili stese za potovanje na lokacijo: ".$destination['title']."</h2>";
+?>
+<div>
+    <h3>Preden odpotujete Vas prosimo, da vnesete nekaj podatkov, da bomo lažje organizirali vaše potovanje.</h3>
+    <form action="travel_add.php" method="POST">
+        Vnesite datum, kdaj nas želite obiskati: <input type="date" name="travel_date" />
+        <input type="hidden" name="destination_id" value="<?php ?>"/>
+    </form>
+</div>
