1d09c455ada76bbc282d4bd21bb96b7543c98a7b
ilion227 <janliber1@gmail.com>
2016-05-19 11:56:15 +0200

    Fixed database

---
+++ insert_statements/turistika_comments.sql
@@ -0,0 +1,4 @@
+INSERT INTO turistika.comments (user_id, destination_id, content, date_add) VALUES (1, 1, 'To je TOP destinacija!', '2015-06-01 07:12:25');
+INSERT INTO turistika.comments (user_id, destination_id, content, date_add) VALUES (2, 1, 'Malo moram popraviti zamike!', '2015-06-01 07:59:45');
+INSERT INTO turistika.comments (user_id, destination_id, content, date_add) VALUES (2, 1, 'asdasdasd', '2015-06-01 07:59:48');
+INSERT INTO turistika.comments (user_id, destination_id, content, date_add) VALUES (2, 1, 'sdfsdfsdf', '2015-06-05 08:50:01');
