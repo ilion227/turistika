117608d9d22208067a632c3c2363d2227e6d9c51
weapon112 <gamerskinoob@gmail.com>
2016-05-25 09:29:40 +0200

    Adding appartment form

--- RSAProjektKlavz/apartment_add.php
+++ RSAProjektKlavz/apartment_add.php
@@ -2,10 +2,31 @@
 include_once 'header.php';
 
 ?>
-        <h1>Dodaj apartma</h1>
-        <?php
-        
-        ?>
+<link href='css/appartment_add_style.css' rel='stylesheet' type='text/css'>
+
+    <form action="appartment_insert.php" method="POST">
+        <div class="wrap">
+            <h1>DODAJ APARTMA</h1>
+            <div> 
+              <input name="title" placeholder="Naslov" type="text" class="cool" required />
+            </div>
+            <div> 
+              Število oseb, ki lahko stanujejo v vašem apartmaju:  <br />
+              <input name="persons" type="number" class="cool" required />
+            </div><br />
+            <div> 
+              CENA(na dan):
+              <input id="price_input"  name="ppd" type="number" class="cool" required /> €
+            </div><br />
+            <div> 
+              Število kopalnic:  <br />
+              <input name="bathrooms" type="number" class="cool" required />
+            </div><br />
+        </div>
+        <div class="wrap2">
+            <h1>DODATNE KATEGORIJE</h1>
+        </div>
+    </form>
         
 <?php
 include_once 'footer.php';
