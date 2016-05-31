00f8eca2f43152b6b75dc1d14fad0e45f3ebcdd0
weapon112 <gamerskinoob@gmail.com>
2016-05-31 13:47:58 +0200

    Adding appartments

--- RSAProjektKlavz/apartment_add.php
+++ RSAProjektKlavz/apartment_add.php
@@ -12,24 +12,78 @@ include_once 'header.php';
             </div>
             <div> 
               Število oseb, ki lahko stanujejo v vašem apartmaju:  <br />
-              <input name="persons" type="number" class="cool" required />
+              <input name="persons" type="number"  required />
             </div><br />
             <div> 
               CENA(na dan):
-              <input id="price_input"  name="ppd" type="number" class="cool" required /> €
+              <input id="price_input"  name="ppd" type="number" required /> €
             </div><br />
             <div style="text-align:center; float:left;margin-right: 20px;"> 
               Število kopalnic  <br />
-              <input name="bathrooms" type="number" class="cool" required />
+              <input name="bathrooms" type="number" required />
             </div>
             <div style="float:left; text-align:center;"> 
               Število spalnic  <br />
-              <input name="bedrooms" type="number" class="cool" required />
+              <input name="bedrooms" type="number" required />
+            </div><br />
+            <div style="text-align:center; float:left;margin-right: 20px;"> 
+              Leto izgradnje
+              <input id="price_input" name="year_made" type="number" required />
+            </div>
+            <div class="input-group" style="text-align:center;margin-left: 20px;">
+                Dostop do interneta:
+                <span class="input-group-addon" >
+                    <input type="checkbox" name="wi-fi">
+                </span>
+              </div>
+            <div style="clear:both;"> 
+              Opis apartmaja  <br />
+              <textarea name="persons" cols="45" rows="5"></textarea>
             </div><br />
         </div>
         <div class="wrap2">
             <h1>DODATNE KATEGORIJE</h1>
+            <div> 
+              <input name="address" placeholder="Ulica" type="text" />
+            </div>
+            <div class="select_div">
+                Mesto: <select name="city_id">
+                <?php
+                $query = "SELECT *
+                          FROM cities;";
+                $result = mysqli_query($link, $query);
+                while ($row = mysqli_fetch_array($result)) {
+                    echo '<option value="'.$row['id'].'">'.$row['title'].'</option>';
+                }
+                ?>
+                 </select>
+            </div>
+            <div class="select_div">
+                Lokacija: <select name="location_id">
+                <?php
+                $query = "SELECT *
+                          FROM locations;";
+                $result = mysqli_query($link, $query);
+                while ($row = mysqli_fetch_array($result)) {
+                    echo '<option value="'.$row['id'].'">'.$row['title'].'</option>';
+                }
+                ?>
+                 </select>
+            </div>
+            <div class="select_div">
+                Kategorija: <select name="category_id">
+                <?php
+                $query = "SELECT *
+                          FROM categories;";
+                $result = mysqli_query($link, $query);
+                while ($row = mysqli_fetch_array($result)) {
+                    echo '<option value="'.$row['id'].'">'.$row['title'].'</option>';
+                }
+                ?>
+                 </select>
+            </div>
         </div>
+        
     </form>
         
 <?php
