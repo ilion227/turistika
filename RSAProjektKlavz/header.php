7e65578e55bf9bab7778e358a3f0ead873b70324
franci98 <franciklavz@gmail.com>
2016-06-05 13:19:36 +0200

    Left nav menu deleted on certain pages

--- RSAProjektKlavz/header.php
+++ RSAProjektKlavz/header.php
@@ -39,3 +39,34 @@ and open the template in the editor.
             </div><!-- .nav-menu -->
         </nav><!-- #site-navigation -->
         <div class="bg-img">
+            <div id="left-nav-menu">
+    <div id="categories-nav-menu">
+        <h2>Kategorije:</h2>
+        <hr>
+        <ul>
+            <?php
+            $query = "SELECT *
+                          FROM categories;";
+                $result = mysqli_query($link, $query);
+                while ($row = mysqli_fetch_array($result)) {
+                    echo '<li>'.$row['title'].'</li>';
+                }
+            ?>
+        </ul> 
+    </div>
+    <div id="categories-nav-menu">
+        <hr>
+        <h2>Lokacije:</h2>
+        <hr>
+        <ul>
+            <?php
+            $query = "SELECT *
+                          FROM locations;";
+                $result = mysqli_query($link, $query);
+                while ($row = mysqli_fetch_array($result)) {
+                    echo '<li>'.$row['title'].'</li>';
+                }
+            ?>
+        </ul> 
+    </div>
+</div>
--- RSAProjektKlavz/index.php
+++ RSAProjektKlavz/index.php
@@ -4,37 +4,7 @@ include_once 'header.php';
 ?>
 <link href='css/index_style.css' rel='stylesheet' type='text/css'>
 
-<div id="left-nav-menu">
-    <div id="categories-nav-menu">
-        <h2>Kategorije:</h2>
-        <hr>
-        <ul>
-            <?php
-            $query = "SELECT *
-                          FROM categories;";
-                $result = mysqli_query($link, $query);
-                while ($row = mysqli_fetch_array($result)) {
-                    echo '<li>'.$row['title'].'</li>';
-                }
-            ?>
-        </ul> 
-    </div>
-    <div id="categories-nav-menu">
-        <hr>
-        <h2>Lokacije:</h2>
-        <hr>
-        <ul>
-            <?php
-            $query = "SELECT *
-                          FROM locations;";
-                $result = mysqli_query($link, $query);
-                while ($row = mysqli_fetch_array($result)) {
-                    echo '<li>'.$row['title'].'</li>';
-                }
-            ?>
-        </ul> 
-    </div>
-</div>
+
 
 
         <?php
